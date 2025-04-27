dfac01ae5af8f1c333c3506932ea3608273be16b
rechard.guo <sdragonguo@163.com>
2025-04-27 16:05:25 +0800

    bolt数据库的crud

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module blockchain-go
 
 go 1.23.4
+
+require (
+	go.etcd.io/bbolt v1.4.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	blk "blockchain-go/blk" // Import the blk package
 	"fmt"
+	"log"
+
+	"go.etcd.io/bbolt"
 )
 
 func main() {
@@ -23,13 +25,68 @@ func main() {
 	// 	fmt.Println()
 	// }
 
-	block := blk.NewBlock(0, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte("Genesis block"))
-	fmt.Printf("%x\n", block.Hash)
-	fmt.Printf("%d\n", block.Nonce)
+	// block := blk.NewBlock(0, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte("Genesis block"))
+	// fmt.Printf("%x\n", block.Hash)
+	// fmt.Printf("%d\n", block.Nonce)
 
-	fmt.Println("--------------------------------")
-	serializedData := block.Serialize()
-	block2 := blk.DeserializeBlock(serializedData)
-	fmt.Printf("%x\n", block2.Hash)
-	fmt.Printf("%d\n", block2.Nonce)
+	// fmt.Println("--------------------------------")
+	// serializedData := block.Serialize()
+	// block2 := blk.DeserializeBlock(serializedData)
+	// fmt.Printf("%x\n", block2.Hash)
+	// fmt.Printf("%d\n", block2.Nonce)
+
+	// Open the database
+	db, err := bbolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Start a writable transaction
+	err = db.Update(func(tx *bbolt.Tx) error {
+		var b *bbolt.Bucket
+		// Get a bucket
+		b = tx.Bucket([]byte("MyBucket"))
+		if b == nil {
+			// Create a bucket
+			bucket, err := tx.CreateBucket([]byte("MyBucket"))
+			if err != nil {
+				return err
+			}
+			b = bucket
+		}
+
+		// Put a key-value pair in the bucket
+		return b.Put([]byte("key"), []byte("value"))
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the value back
+	err = db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		if b == nil {
+			return fmt.Errorf("bucket not found")
+		}
+
+		v := b.Get([]byte("key"))
+		fmt.Printf("Value: %s\n", v)
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete the key-value pair
+	err = db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		if b == nil {
+			return fmt.Errorf("bucket not found")
+		}
+
+		return b.Delete([]byte("key"))
+	})
 }
