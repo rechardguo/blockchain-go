ac4d04a2ce7d7f85d12cd76a16446f09670cd3c1
rechard.guo <sdragonguo@163.com>
2025-04-27 12:09:30 +0800

    添加新的区块到区块链里

--- blk/blockchain.go
+++ blk/blockchain.go
@@ -4,6 +4,11 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
+func (bc *Blockchain) AddBlockToBlockChain(data string) {
+	newBlock := NewBlock(int64(len(bc.Blocks)), bc.Blocks[len(bc.Blocks)-1].Hash, []byte(data))
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
 func CreateBlockChainWithGenesisBlock() *Blockchain {
 
 	genesisBlk := CreateGenesisBlock("Genesis Block")
--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 )
 
 func main() {
-	block := blk.CreateBlockChainWithGenesisBlock()
-	fmt.Println(block.Blocks[0])
+	blockchain := blk.CreateBlockChainWithGenesisBlock()
+	fmt.Println(blockchain.Blocks[0])
 
+	blockchain.AddBlockToBlockChain("block1")
+	blockchain.AddBlockToBlockChain("block2")
+	blockchain.AddBlockToBlockChain("block3")
+	blockchain.AddBlockToBlockChain("block4")
+
+	for _, block := range blockchain.Blocks {
+		fmt.Printf("Height: %d\n", block.Height)
+		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
